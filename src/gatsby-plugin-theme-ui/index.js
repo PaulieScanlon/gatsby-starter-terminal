d0bbfcf50329f167a800e511fe2c1b31e4741433
pauliescanlon <pauliescanlon@gmail.com>
2020-02-26 12:59:47 +0530

    add new theme styles

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,18 +1,26 @@
-import codeTheme from "@theme-ui/prism/presets/vs-dark.json";
+import codeTheme from "@theme-ui/prism/presets/night-owl-light.json";
 import baseTheme from "@pauliescanlon/gatsby-theme-terminal/src/gatsby-plugin-theme-ui";
 
 export default {
   ...baseTheme,
   colors: {
     ...baseTheme.colors,
+    text: "#222222",
+    muted: "#666666",
     primary: "#FF4081",
     secondary: "#03A9F4",
     success: "#FFEB3B",
-    background: "#232323",
-    surface: "#393939"
+    background: "#ffffff",
+    surface: "#f3f3f3"
   },
   styles: {
     ...baseTheme.styles,
+    p: {
+      code: {
+        ...baseTheme.styles.p.code,
+        color: "inherit"
+      }
+    },
     pre: {
       ...baseTheme.styles.pre,
       ...codeTheme
